08349b42bb45b0fadb48a4361828b72b6a88caea
markmcwhirter <mark.mcwhirter@tesco.com>
2019-09-08 11:39:35 +0100

    Add Sport Category

--- stream/admin.py
+++ stream/admin.py
@@ -37,24 +37,20 @@ change_cat_MUSIC.short_description = "Change Category to Music"
 
 def change_cat_NEWS(modeladmin, request, queryset):
     queryset.update(channel_group='NEWS')
-change_cat_NEWS.short_description = "Change Category to NEWS"
-
-def change_cat_SPORTS(modeladmin, request, queryset):
-    queryset.update(channel_group='SPORTS')
-change_cat_SPORTS.short_description = "Change Category to SPORTS"
-
-def change_cat_TRAVEL(modeladmin, request, queryset):
-    queryset.update(channel_group='TRAVEL')
-change_cat_TRAVEL.short_description = "Change Category to Travel"
+change_cat_NEWS.short_description = "Change Category to News"
 
 def change_cat_SPORTS(modeladmin, request, queryset):
     queryset.update(channel_group='SPORTS')
 change_cat_SPORTS.short_description = "Change Category to Sports"
 
+def change_cat_TRAVEL(modeladmin, request, queryset):
+    queryset.update(channel_group='TRAVEL')
+change_cat_TRAVEL.short_description = "Change Category to Travel"
+
 class ChannelAdmin(admin.ModelAdmin):
     list_display = ('epg_id', 'channel_name','channel_group','channel_enabled')
     ordering = ('epg_id', 'id')
-    actions = [enable_channel, disable_channel,change_cat_ENTERTAINMENT,change_cat_COMEDY,change_cat_DRAMA,change_cat_MOVIES,change_cat_MUSIC,change_cat_NEWS,change_cat_SPORTS,change_cat_TRAVEL,change_cat_SPORTS]
+    actions = [enable_channel, disable_channel,change_cat_ENTERTAINMENT,change_cat_COMEDY,change_cat_DRAMA,change_cat_MOVIES,change_cat_MUSIC,change_cat_NEWS,change_cat_SPORTS,change_cat_TRAVEL]
 
 class ProgrammeAdmin(admin.ModelAdmin):
     list_display = ('Channel', 'Title', 'Start')
